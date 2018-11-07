4cb97c65792fae3c27e69bd7549502e92bd8d97f
Mark Vasilkov <mvasilkov@gmail.com>
2018-11-07 13:44:11 +0200

    UTF-8

--- test/test_wasm.js
+++ test/test_wasm.js
@@ -3,14 +3,14 @@ Module.onRuntimeInitialized = function () {
 
     assert(Module.usingWasm)
 
-    const utf8 = (new TextEncoder).encode('oh hai')
+    const utf8 = (new TextEncoder).encode('превет')
 
     const buf_pointer = Module.ccall('get_buf', 'number')
     const u32view = new Uint32Array(Module.wasmMemory.buffer, buf_pointer, 1)
-    const u8view = new Uint8Array(Module.wasmMemory.buffer, buf_pointer + 4, 6)
+    const u8view = new Uint8Array(Module.wasmMemory.buffer, buf_pointer + 4, utf8.length)
 
-    u32view[0] = 6
-    for (let n = 0; n < 6; ++n)
+    u32view[0] = utf8.length
+    for (let n = 0; n < utf8.length; ++n)
         u8view[n] = utf8[n]
 
     Module.ccall('print_blake2b')
